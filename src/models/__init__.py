26a8bda0c3e684363e8a8605fd33b2fb189fe982
Nuno Lourenço <nlourenco@lx.it.pt>
2022-07-26 12:29:46 +0100

    added AidaEmModel to model init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 
-from .passives import PassivesModel
+from .passives import PassivesModel, AidaEmModel
 from .train_test_util import load_data, test_it, error_to_csv
 
 from .models_scipy import RBFInterpolatorModel, GaussianProcessRegressorModel
